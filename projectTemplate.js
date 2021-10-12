7d67c817d93ccbf6344cdffda7f092ec48808328
cyber-ikegami <ikegami@cyberbeing.tech>
2021-10-12 13:09:04 +0900

    1012進捗状況_01

--- projectTemplate.js
+++ projectTemplate.js
@@ -3,7 +3,7 @@ function outputResult() {
 	const value = window.getSelection();
 	const resultAreaValue = document.getElementById("resultArea").value;
 
-    resultForm.resultArea.value =
+    resultForm.resultArea.value = 
 	`${resultAreaValue}
 	2021/mm/dd 所属）名前
 	
