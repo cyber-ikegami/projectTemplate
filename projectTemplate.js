82cbf6956d2865f7d9b5bfeca39ec9256b040087
cyber-ikegami <ikegami@cyberbeing.tech>
2021-10-15 10:22:14 +0900

    1015進捗状況_01

--- projectTemplate.js
+++ projectTemplate.js
@@ -1,29 +1,34 @@
 //入力必須チェック
 function inputRequiredCheck() {
-	// const selectValue = window.getSelection();
 	const inputAreaValue = document.getElementById("inputArea").value;
 	const extractButton = document.getElementById("extractButton");
-	const resultArea = document.getElementById("resultArea");
-	
+
 	extractButton.disabled = (inputAreaValue == "");
-	resultArea.disabled = (inputAreaValue == "");
 }
 
 // リセット処理
-// 選択した文字もリセットしたい～～
 function resetForm() {
+	const resetButton = document.getElementById("resetButton");
+	const resultArea = document.getElementById("resultArea");
+	resetButton.disabled = true;
+	resultArea.disabled = true;
+
 	document.getElementById("resultArea").value = "";
 }
 
 // 出力処理
 function outputResult() {
 	const selectValue = window.getSelection();
+	const resetButton = document.getElementById("resetButton");
 	let resultArea = document.getElementById("resultArea");
-
+	resultArea.disabled = false;
+	
     resultForm.resultArea.value = 
 	`${resultArea.value}2021/mm/dd 所属）名前
 	
 	${selectValue}
 	--------------------------------------------
 	` .replace(/[ \t\r]+/g, "");
+
+	resetButton.disabled = (resultArea.value == "");
 }
